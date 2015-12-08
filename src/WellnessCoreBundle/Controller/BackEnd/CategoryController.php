dae67d5809b93e16a6a1a19e80b78a4a6d2255e6
yohan <heyvaertyohan@gmail.com>
2015-12-08 16:49:00 +0100

    fos user

--- src/WellnessCoreBundle/Controller/BackEnd/CategoryController.php
+++ src/WellnessCoreBundle/Controller/BackEnd/CategoryController.php
@@ -3,12 +3,36 @@
 namespace WellnessCoreBundle\Controller\BackEnd;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use WellnessCoreBundle\Entity\CategoryService;
+use WellnessCoreBundle\Entity\Image;
 
 class CategoryController extends Controller
 {
     public function listAction()
     {
-        return $this->render('WellnessCoreBundle:BackEnd/Category:list.html.twig');
+        $rep_img = $this->getDoctrine()->getManager()->getRepository('WellnessCoreBundle:Image');
+        $listImagesSlider = $rep_img->findBy(array('type' => 'mainSlider'));
+        $listCategoryWithLogo = $rep_img->getListImages(5, Image::TYPE_LOGO_SERVICE, null);
+
+        $listCategories = $this->getDoctrine()->getEntityManager()->getRepository('WellnessCoreBundle:CategoryService')->findAll();
+
+        if ($listImagesSlider == null) {
+            throw $this->createNotFoundException("Pas d'image trouvée !");
+        }
+
+        if ($listCategories == null) {
+            throw $this->createNotFoundException("Pas de categorie trouvée !");
+        }
+
+        if ($listCategoryWithLogo == null) {
+            throw $this->createNotFoundException("Pas de logo prestataire trouvé !");
+        }
+
+        return $this->render('WellnessCoreBundle:BackEnd/Category:list.html.twig', array(
+            'listCategoryWithLogo' => $listCategoryWithLogo,
+            'listCategories' => $listCategories,
+            'listImagesSlider' => $listImagesSlider
+        ));
     }
 
     public function showAction()
--- src/WellnessCoreBundle/Resources/public/js/backend.js
+++ src/WellnessCoreBundle/Resources/public/js/backend.js
@@ -1,6 +1,38 @@
-$('#li_service').click(function () {
+$('document').ready(function(){
+    menuManagement();
+
+
+
+});
+
+function menuManagement() {
+    var menu_ul = $('.menu > li > ul'),
+        menu_a = $('.menu > li > a');
+
+    menu_ul.hide();
+
+    menu_a.click(function (e) {
+        e.preventDefault();
+
+        if (!$(this).hasClass('active')) {
+            menu_a.removeClass('active');
+            menu_ul.filter(':visible').slideUp('normal');
+            $(this).addClass('active').next().stop(true, true).slideDown('normal');
+        } else {
+            $(this).removeClass('active');
+            $(this).next().stop(true, true).slideUp('normal');
+        }
+    });
+
+}
+
+
+
+
+
+/*$('#li_service').click(function () {
     $(this).parent().siblings().find("ul").slideUp("normal"); // Slide up all sub menus except the one clicked
     $(this).next().slideToggle("normal"); // Slide down the clicked sub menu
     $('#li_service').css('backgroundColor', 'red');
     $(this).css('backgroundColor', '#009F8B');
-});
+});*/
